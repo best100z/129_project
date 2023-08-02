3aeacbcb4c44e7f6e74e75e45a3a31cbbed2cb52
best100z <95509146+best100z@users.noreply.github.com>
2023-08-01 23:37:12 -0700

    Add files via upload

---
+++ Sensor_fit.py
@@ -0,0 +1,35 @@
+import time
+import numpy as np; 
+import board;
+import busio;
+import adafruit_ads1x15.ads1015 as ADS;
+from adafruit_ads1x15.analog_in import AnalogIn;
+###
+dis = 15;
+time.sleep(0.4);
+ACQTIME = 0.1;
+SPS = 920;
+VGAIN = 1
+nsamples = int(ACQTIME*SPS)
+sinterval = 1.0/SPS
+i2c = busio.I2C(board.SCL, board.SDA);
+ads = ADS.ADS1015(i2c)
+channel = AnalogIn(ads, ADS.P0);
+ads.gain = VGAIN;
+ads.data_rate = SPS
+ads.mode = ADS.Mode.CONTINUOUS
+indata = np.zeros(nsamples,'float')
+vin = AnalogIn(ads, 0)
+for i in range(nsamples):
+	st = time.perf_counter()
+	indata[i] = vin.voltage
+	while (time.perf_counter() - st) <= sinterval:
+		pass
+with open("/home/zky/project_129/data.txt","a") as fil:
+	print(indata.sum()/len(indata), dis, file = fil);
+	"""
+	for i,val in enumerate(indata):
+		if val >=0.46:
+			indata[i] = np.log((val-0.46)/3.67)/(-0.068)    
+	print(indata, file = fil);
+	"""
