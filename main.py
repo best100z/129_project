ae108819206dffa9ab64d6ed458374c0e8d3a074
best100z <95509146+best100z@users.noreply.github.com>
2023-08-01 21:59:25 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+import subprocess;
+
+subprocess.run(["python3", "/home/kaiyanzhou/project_129/Motor.py"]);
+subprocess.run(["python3", "/home/kaiyanzhou/project_129/Sensor.py"]);
+subprocess.run(["python3", "/home/kaiyanzhou/project_129/Motor.py"]);
